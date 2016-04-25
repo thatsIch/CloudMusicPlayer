3efe3d8c4211c8b6cbe7409f792d069751d694e2
thatsIch <thatsIch@mail.de>
2016-04-25 21:23:40 +0200

    Added all possible Sound Cloud API Endpoints

---
+++ src/main/java/de/thatsich/soundcloud/api/AppsEndpoint.java
@@ -0,0 +1,23 @@
+package de.thatsich.soundcloud.api;
+
+
+/**
+ * TODO add description
+ *
+ * TODO add meaning
+ *
+ * TODO add usage
+ *
+ * @author thatsIch (thatsich[at]mail[dot]de)
+ * @version 1.0.0-SNAPSHOT
+ * @since 1.0.0-SNAPSHOT 25.04.2016
+ */
+public class AppsEndpoint implements Endpoint
+{
+	private static final String ENDPOINT_PATH = "/apps";
+
+	public String getEndPointPath()
+	{
+		return ENDPOINT_PATH;
+	}
+}
---
+++ src/main/java/de/thatsich/soundcloud/api/Endpoint.java
@@ -0,0 +1,18 @@
+package de.thatsich.soundcloud.api;
+
+
+/**
+ * TODO add description
+ *
+ * TODO add meaning
+ *
+ * TODO add usage
+ *
+ * @author thatsIch (thatsich[at]mail[dot]de)
+ * @version 1.0.0-SNAPSHOT
+ * @since 1.0.0-SNAPSHOT 25.04.2016
+ */
+public interface Endpoint
+{
+	String getEndPointPath();
+}
